3cb5e12cc648b454e9079350dbb1a95df14569f9
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-13 09:32:59 +0300

    Add files via upload

---
+++ 1. FirstStepsInProgramming/5. Greeting by Name.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _5._Greeting_by_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            Console.WriteLine("Hello, " + a + "!");
+        }
+    }
+}
