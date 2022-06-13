b9fe597a8b84728b84dba17fc6930f3c8863d251
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-13 10:54:12 +0300

    Add files via upload

---
+++ 2. FirstStepsInProgrammingExercise/7. Food Delivery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _7._Food_Delivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double d = a * 10.35 + b * 12.40 + c * 8.15;
+            double e = d * 1/5;
+
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+
+            Console.WriteLine(d + e + 2.50);
+        }
+    }
+}
