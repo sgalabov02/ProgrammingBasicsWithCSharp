0d2da61bbe9abb062a45dcedc708d4d3014e4f5f
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-13 09:41:41 +0300

    Add files via upload

---
+++ 1. FirstStepsInProgramming/7. Projects Creation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _7._Projects_Creation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int b = int.Parse(Console.ReadLine());
+            int c = b * 3;
+
+            Console.WriteLine("The architect " + a + " will need " + c + " hours to complete " + b + " project/s.");
+        }
+    }
+}
