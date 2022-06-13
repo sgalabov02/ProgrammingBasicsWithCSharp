b76e50120e3bd6056f26ba4dcd37cf60d27d76b6
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-13 10:59:46 +0300

    Add files via upload

---
+++ 2. FirstStepsInProgrammingExercise/8. Basketball Equipment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _8._Basketball_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = a - a * 40 / 100;
+            double c = b - b * 20 / 100;
+            double d = c * 1 / 4;
+            double e = d * 1 / 5;
+            double f = a + b + c + d + e;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+        }
+    }
+}
