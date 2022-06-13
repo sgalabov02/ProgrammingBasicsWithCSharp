96e4c21ec5e1cd64e7c234881a0c5012fd90b209
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-13 11:31:46 +0300

    Add files via upload

---
+++ 3. ConditionalStatements/6. Speed Info.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _6._Speed_Info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
