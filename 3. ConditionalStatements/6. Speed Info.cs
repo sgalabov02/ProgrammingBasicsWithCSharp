30280efab7f2212d8e2d19d3ba24757062af0a5d
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-13 11:32:20 +0300

    Update 6. Speed Info.cs

--- 3. ConditionalStatements/6. Speed Info.cs
+++ 3. ConditionalStatements/6. Speed Info.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace _6._Speed_Info
 {
@@ -6,7 +6,28 @@ namespace _6._Speed_Info
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a = double.Parse(Console.ReadLine());
+
+            if (a <= 10)
+            {
+                Console.WriteLine("slow");
+            }
+            else if (a > 10 && a <= 50)
+            {
+                Console.WriteLine("average");
+            }
+            else if (a > 50 && a <= 150)
+            {
+                Console.WriteLine("fast");
+            }
+            else if (a > 150 && a <= 1000)
+            {
+                Console.WriteLine("ultra fast");
+            }
+            else if (a > 1000)
+            {
+                Console.WriteLine("extremely fast");
+            }
         }
     }
 }
