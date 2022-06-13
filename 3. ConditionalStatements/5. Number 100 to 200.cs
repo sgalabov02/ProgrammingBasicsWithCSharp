aa96e6acb3689e22c1a881dec6b2f0af6def0398
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-13 11:26:31 +0300

    Add files via upload

---
+++ 3. ConditionalStatements/5. Number 100 to 200.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _5._Number_100_to_200
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            if(a < 100)
+            {
+                Console.WriteLine("Less than 100");
+            }
+            else if(a > 100 && a < 200)
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+            else if(a > 200)
+            {
+                Console.WriteLine("Greater than 200");
+            }
+        }
+    }
+}
