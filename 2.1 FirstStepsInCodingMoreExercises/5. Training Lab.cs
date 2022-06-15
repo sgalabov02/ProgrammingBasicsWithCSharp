3e110d437e23cc3750060159c4522690867df317
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-15 22:37:27 +0300

    Add files via upload

---
+++ 2.1 FirstStepsInCodingMoreExercises/5. Training Lab.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _5._Training_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double w = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double a = Math.Floor(w * 100 / 120);
+            double b = Math.Floor((h * 100 - 100) / 70);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            double seats = a * b - 3;
+
+
+            Console.WriteLine(seats);
+        }
+    }
+}
