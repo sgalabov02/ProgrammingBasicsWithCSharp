c981f5e51d68f5736c3a052e7d21e4fcbae73608
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-15 23:02:43 +0300

    Add files via upload

---
+++ 2.1 FirstStepsInCodingMoreExercises/9. Weather Forecast.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _9._Weather_Forecast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string condition = Console.ReadLine();
+
+            if (condition == "sunny" || '["sunny"]')
+            {
+                Console.WriteLine("It's warm outside!");
+            }
+            else
+            {
+                Console.WriteLine("It's cold outside!");
+            }
+        }
+    }
+}
