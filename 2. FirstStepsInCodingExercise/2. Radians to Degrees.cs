21c95c91a6343cbd418c9304e7a44c4f1b088223
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-16 00:05:41 +0300

    Add files via upload

---
+++ 2. FirstStepsInCodingExercise/2. Radians to Degrees.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2._Radians_to_Degrees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = a * 180 / Math.PI;
+
+            Console.WriteLine(b);
+        }
+    }
+}
