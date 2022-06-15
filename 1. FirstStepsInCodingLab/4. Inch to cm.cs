8882569f97a34511674bfb9e9f8566774916822a
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-15 23:58:36 +0300

    Add files via upload

---
+++ 1. FirstStepsInCodingLab/4. Inch to cm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4._Inches_to_cm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = a * 2.54;
+
+            Console.WriteLine(b);
+
+        }
+    }
+}
