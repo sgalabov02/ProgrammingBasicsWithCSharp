cdd4251758f12f05f5e526608474e125301d6060
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-16 00:20:31 +0300

    Add files via upload

---
+++ 4. ConditionalStatementsLab/6. Speed Info.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _6._Speed_Info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+
+            if (a <= 10)
+            {
+                Console.WriteLine("slow");
+            }
+            else if (a > 10 && a <= 50)
+            {
+                Console.WriteLine("average");
+            }
+            else if (a > 50 && a <= 150)
+            {
+                Console.WriteLine("fast");
+            }
+            else if (a > 150 && a <= 1000)
+            {
+                Console.WriteLine("ultra fast");
+            }
+            else if (a > 1000)
+            {
+                Console.WriteLine("extremely fast");
+            }
+        }
+    }
+}
