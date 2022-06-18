45aea9fcaa8caa2691a845e4d328902003c96da1
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-18 20:28:01 +0300

    Add files via upload

---
+++ 8. ConditionalStatementsAdvancedExercise/1. Cinema.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1._Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string projection = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+            double price = 0;
+
+            if (projection == "Premiere")
+            {
+                price = 12.00;
+            }
+            else if(projection == "Normal")
+            {
+                price = 7.50;
+            }
+            else if(projection == "Discount")
+            {
+                price = 5.00;
+            }
+            Console.WriteLine($"{price * rows * columns:f2} leva");
+        }
+    }
+}
