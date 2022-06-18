f6abb84b955e7d5ddd5a0f9291976bc86785ec65
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-18 13:33:30 +0300

    Add files via upload

---
+++ 5. ConditionalStatementsExercise/7. Shopping.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _7._Shopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int graphics_cards = int.Parse(Console.ReadLine());
+            int processors = int.Parse(Console.ReadLine());
+            int ram = int.Parse(Console.ReadLine());
+
+            double sum_graphics_cards = graphics_cards * 250;
+            double sum_processors = sum_graphics_cards * 35 / 100;
+            double processors_price = sum_processors * processors;
+            double sum_ram = sum_graphics_cards * 10 / 100;
+            double ram_price = sum_ram * ram;
+            double total_sum = sum_graphics_cards + processors_price + ram_price;
+            if(graphics_cards > processors)
+            {
+                total_sum = total_sum - total_sum * 15 / 100;
+            }
+            if(total_sum < budget)
+            {
+                Console.WriteLine($"You have {budget - total_sum:f2} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {total_sum - budget:f2} leva more!");
+            }
+        }
+    }
+}
