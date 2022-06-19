9c393dece5fd59aedb84baf245b48a16f2021137
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-19 07:48:29 +0300

    Add files via upload

---
+++ 8. ConditionalStatementsAdvancedExercise/4. Fishing Boat.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace _4._Fishing_Boat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int budget = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            int fishermen = int.Parse(Console.ReadLine());
+            double price = 0;
+
+            switch(season)
+            {
+                case "Spring":
+                    price = 3000;
+                    if (fishermen <= 6)
+                    {
+                        price = price - price * 10 / 100;
+                    }
+                    else if(fishermen >= 7 && fishermen <= 11)
+                    {
+                        price = price - price * 15 / 100;
+                    }
+                    else if(fishermen >= 12)
+                    {
+                        price = price - price * 25 / 100;
+                    }
+                    if (fishermen % 2 == 0)
+                    {
+                        price = price - price * 5 / 100;
+                    }
+                    break;
+                case "Summer":
+                    price = 4200;
+                    if (fishermen <= 6)
+                    {
+                        price = price - price * 10 / 100;
+                    }
+                    else if (fishermen >= 7 && fishermen <= 11)
+                    {
+                        price = price - price * 15 / 100;
+                    }
+                    else if (fishermen >= 12)
+                    {
+                        price = price - price * 25 / 100;
+                    }
+                    if (fishermen % 2 == 0)
+                    {
+                        price = price - price * 5 / 100;
+                    }
+                    break;
+                case "Autumn":
+                    price = 4200;
+                    if (fishermen <= 6)
+                    {
+                        price = price - price * 10 / 100;
+                    }
+                    else if (fishermen >= 7 && fishermen <= 11)
+                    {
+                        price = price - price * 15 / 100;
+                    }
+                    else if (fishermen >= 12)
+                    {
+                        price = price - price * 25 / 100;
+                    }
+                    break;
+                case "Winter":
+                    price = 2600;
+                    if (fishermen <= 6)
+                    {
+                        price = price - price * 10 / 100;
+                    }
+                    else if (fishermen >= 7 && fishermen <= 11)
+                    {
+                        price = price - price * 15 / 100;
+                    }
+                    else if (fishermen >= 12)
+                    {
+                        price = price - price * 25 / 100;
+                    }
+                    if (fishermen % 2 == 0)
+                    {
+                        price = price - price * 5 / 100;
+                    }
+                    break;
+                default:
+                    return;
+            }
+            if (budget >= price)
+            {
+                Console.WriteLine($"Yes! You have {budget - price:f2} leva left.");
+            }
+            else if (price > budget)
+            {
+                Console.WriteLine($"Not enough money! You need {price - budget:f2} leva.");
+            }
+        }
+    }
+}
