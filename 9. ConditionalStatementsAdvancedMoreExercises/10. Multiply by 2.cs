fef967253f93af7e1d7c97acdf034c648a57a3b5
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-20 13:03:38 +0300

    Add files via upload

---
+++ 9. ConditionalStatementsAdvancedMoreExercises/10. Multiply by 2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _10._Multiply_by_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0;
+            while (a >= 0)
+            {
+                a = double.Parse(Console.ReadLine());
+                if (a >= 0)
+                {
+                    Console.WriteLine($"Result: {a * 2:f2}");
+                }
+                else
+                {
+                    Console.WriteLine("Negative number!");
+                }
+            }
+        }
+    }
+}
