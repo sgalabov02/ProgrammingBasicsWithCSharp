315eaf06515f46ba728f4a3f22226fefa69a57b1
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-20 12:57:17 +0300

    Add files via upload

---
+++ 9. ConditionalStatementsAdvancedMoreExercises/9. Numbers from 1 to 10.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _9._Numbers_from_1_to_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            while (a < 10)
+            {
+                a++;
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
