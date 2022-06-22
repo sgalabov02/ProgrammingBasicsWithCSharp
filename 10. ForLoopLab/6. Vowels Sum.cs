812549dd10c88780a2e126fd99ab002abc438c9a
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-22 06:59:35 +0300

    Add files via upload

---
+++ 10. ForLoopLab/6. Vowels Sum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _5._Character_sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                char letter = a[i];
+                //Console.WriteLine(letter);
+                if (letter == 'a')
+                {
+                    sum += 1;
+                }
+                else if (letter == 'e')
+                {
+                    sum += 2;
+                }
+                else if (letter == 'i')
+                {
+                    sum += 3;
+                }
+                else if (letter == 'o')
+                {
+                    sum += 4;
+                }
+                else if (letter == 'u')
+                {
+                    sum += 5;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
