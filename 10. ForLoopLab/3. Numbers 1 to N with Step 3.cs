27a58eddb7a6507e05646ae8344f2e15b466093a
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-22 06:20:58 +0300

    Add files via upload

---
+++ 10. ForLoopLab/3. Numbers 1 to N with Step 3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _3._Numbers_1_to_N_with_Step_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i+=3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
