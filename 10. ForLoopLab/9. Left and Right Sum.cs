ac4d07afaae68af6d0223c03dc4aae329d5c5a7b
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-22 07:44:40 +0300

    Add files via upload

---
+++ 10. ForLoopLab/9. Left and Right Sum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _9._3_Left_and_Right_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
