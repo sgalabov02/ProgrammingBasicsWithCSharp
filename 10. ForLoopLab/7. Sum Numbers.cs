de3177a51fc7a5829c38c94c95e5e07fcc1c0119
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-22 06:36:29 +0300

    Add files via upload

---
+++ 10. ForLoopLab/7. Sum Numbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _7._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int c = 0;
+
+            for (int i = 0; i < a; i++)
+            {
+                int b = int.Parse(Console.ReadLine());
+                c += b;
+            }
+            Console.WriteLine(c);
+        }
+    }
+}
