c52cd017f8b909b9748d17be34cb81b0041947e5
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-22 06:18:47 +0300

    Add files via upload

---
+++ 10. ForLoopLab/2. Numbers N to 1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2._Numbers_N_to_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = N; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
