c279b7285044afe806c7e24eceef0bb4661adae8
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-22 06:29:47 +0300

    Add files via upload

---
+++ 10. ForLoopLab/5. Character sequence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _5._Character_sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                char letter = a[i];
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
