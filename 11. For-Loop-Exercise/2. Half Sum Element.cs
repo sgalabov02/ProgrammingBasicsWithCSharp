0679ab49d31c8648557bece42f9478520aeb3b87
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-23 13:37:12 +0300

    Add files via upload

---
+++ 11. For-Loop-Exercise/2. Half Sum Element.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _2._Half_Sum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int max = int.MinValue;
+            int a = int.Parse(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                int b = int.Parse(Console.ReadLine());
+                sum += b;
+
+                if (b > max)
+                {
+                    max = b;
+                }
+            }
+            int sum2 = sum - max;
+            if (max == sum2)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + max);
+            }
+            else
+            {
+                int diff = Math.Abs(max - sum2);
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + diff);
+            }
+        }
+    }
+}
